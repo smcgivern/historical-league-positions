976bf7ed1d1952b99167f23e05f97c66f52ab77b
Sean McGivern <sean@mcgivern.me.uk>
2023-06-02 15:55:58 +0100

    Use Nix and bump Ruby version

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem(
+    system:
+    let pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShellNoCC {
+        buildInputs = with pkgs; [ ruby_3_2 ];
+      };
+    }
+  );
+}
